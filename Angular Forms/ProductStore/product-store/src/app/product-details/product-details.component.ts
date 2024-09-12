5440219158b75dca18b352eccf346a29d3166657
oddify <omkardali5@gmail.com>
2024-09-12 23:13:09 +0530

    commit before joining ibm

--- Angular Forms/ProductStore/product-store/src/app/product-details/product-details.component.ts
+++ Angular Forms/ProductStore/product-store/src/app/product-details/product-details.component.ts
@@ -4,18 +4,41 @@ import {ProductService} from "../services/product.service";
 import {CurrencyPipe} from "@angular/common";
 import {CommonModule} from "@angular/common";
 import {ActivatedRoute, Router, RouterLink} from "@angular/router";
+import {MatTableModule} from '@angular/material/table';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 @Component({
   selector: 'app-product-details',
   standalone: true,
   imports: [
-    CurrencyPipe, CommonModule, RouterLink
+    CurrencyPipe, CommonModule, RouterLink,MatTableModule
   ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 
 })
 export class ProductDetailsComponent {
+  displayedColumns: string[] = ['demo-position', 'demo-name', 'demo-weight', 'demo-symbol'];
+  dataSource = ELEMENT_DATA;
+
   products: IProduct[]=[];
   errorMsg: string='';
   private successMsg: string = '';
